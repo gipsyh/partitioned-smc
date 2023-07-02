baaf0293434829649fb877e0035919469d256910
Yuheng Su <gipsyh.icu@gmail.com>
2023-07-03 02:18:03 +0800

    update

--- src/traditional.rs
+++ src/traditional.rs
@@ -10,6 +10,7 @@ pub fn check(manager: BddManager, smv: Smv, args: Args) -> (bool, Duration) {
         BuchiAutomata::from_ltl(ltl, &manager, &smvbdd.symbols, &smvbdd.defines).to_fsmbdd();
     let product = fsmbdd.product(&ltl_fsmbdd);
 
+    println!("traditional smc begin");
     let start = Instant::now();
     let forward = product.reachable_from_init();
     let fair_cycle = product.fair_cycle_with_constrain(&forward);
